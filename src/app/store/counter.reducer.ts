f29e1cfc22ec3fac81ee5cd60b4a2f05fb6ce759
kovacsaronlaszlo <k.aron.laszlo@gmail.com>
2024-05-07 15:05:20 +0200

    other way to create a reducer

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -2,4 +2,8 @@ import { createReducer } from "@ngrx/store";
 
 const initialState = 0; // lehet object, array, string, boolean, és number
 
-export const counterReducer = createReducer(initialState);
+//export const counterReducer = createReducer(initialState);
+
+export function counterReducer(state = initialState) {
+    return state
+}
