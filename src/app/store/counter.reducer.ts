ea84dcd33a80b68e94d0f970fc67f65ef3c543e9
kovacsaronlaszlo <k.aron.laszlo@gmail.com>
2024-05-07 15:40:38 +0200

    dispatching action

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,9 +1,14 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from '@ngrx/store';
+import { decrement, increment } from './counter.actions';
 
 const initialState = 0; // lehet object, array, string, boolean, és number
 
-//export const counterReducer = createReducer(initialState);
+export const counterReducer = createReducer(
+  initialState,
+  on(increment, (state) => state + 1),
+  on(decrement, (state) => state - 1)
+);
 
-export function counterReducer(state = initialState) {
+/* export function counterReducer(state = initialState) {
     return state
-}
+} */
