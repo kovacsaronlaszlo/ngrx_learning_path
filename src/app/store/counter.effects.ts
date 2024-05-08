060e9e9bde27f5b8b330ab8c0a898627cf180a1e
kovacsaronlaszlo <k.aron.laszlo@gmail.com>
2024-05-08 10:36:43 +0200

    usse data from lovalStorage and set it into store

--- src/app/store/counter.effects.ts
+++ src/app/store/counter.effects.ts
@@ -1,12 +1,26 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { decrement, increment } from './counter.actions';
-import { tap, withLatestFrom } from 'rxjs';
+import { decrement, increment, init, set } from './counter.actions';
+import { of, switchMap, tap, withLatestFrom } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { selectCount } from './counter.selectors';
 
 @Injectable()
 export class CounterEffects {
+  loadCount = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(init),
+        switchMap(() => {
+          const storedCounter = localStorage.getItem('count');
+          if (storedCounter) {
+            return of(set({ value: +storedCounter }));
+          }
+          return of(set({ value: 0 }));
+        })
+      ) // nem kapja meg a "{ dispatch: false }", mmivel normál módon működik az effect
+  );
+
   saveCount = createEffect(
     () =>
       this.actions$.pipe(
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,12 +1,16 @@
 import { createReducer, on } from '@ngrx/store';
-import { decrement, increment } from './counter.actions';
+import { decrement, increment, set } from './counter.actions';
 
 const initialState = 0; // lehet object, array, string, boolean, és number
 
 export const counterReducer = createReducer(
   initialState,
-  on(increment, (state, action) => state + action.value /* => must always be syncronous */), 
-  on(decrement, (state, action) => state - action.value)
+  on(
+    increment,
+    (state, action) => state + action.value /* => must always be syncronous */
+  ),
+  on(decrement, (state, action) => state - action.value),
+  on(set, (state, action) => action.value)
 );
 
 /* export function counterReducer(state = initialState, action: CounterActions | Action) {
